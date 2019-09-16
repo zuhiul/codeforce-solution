df6f096bc992e9ba12dbe3e716d27e9515f1e94b
xuzinan <xuzinan@bytedance.com>
2019-09-16 17:25:33 +0800

    add 1215E.cpp
    Change-Id: I33749eaa6bc33dce6c6956aed6b9c355b149d8d1

--- 1215/e.cpp
+++ 1215/e.cpp
@@ -1,5 +1,53 @@
-#include<bits/stdc++.h>
+#include<algorithm>
+#include<iostream>
+#include<vector>
 using namespace std;
+int n;
+const int maxm = 1e6;
+const long long inf = 1e18;
+vector<long long> mat[22];
+long long value[22][22];
+long long ans[1<<21];
 int main(){
-	return 0;
+    scanf("%d",&n);
+    for(int i = 0, a;i<n;i++) {
+        scanf("%d",&a);
+        mat[a].push_back(i);
+    }
+    for(int i = 0;i<=20;i++) mat[i].push_back(maxm<<1);
+    for(int i = 1;i<=20;i++) {
+        if(mat[i].size()==0) continue;
+        for(int j = 1;j<=20;j++) {
+            if(mat[j].size()==0||i==j) continue;
+            long long ind1 = 0;
+            for(auto ind : mat[j]) {
+                if(ind > maxm) continue;
+                while(ind > mat[i][ind1]) ind1++;
+                value[i-1][j-1] += ind1;
+            }
+        }
+    }
+////for(int i = 0;i<=4;i++) {
+////    for(int j = 0;j<=4;j++) {
+////        cout<<value[i][j]<<' ';
+////    }
+////    cout<<endl;
+////}
+    for(int i = 0;i<(1<<20);i++) ans[i] = inf;
+    ans[0] = 0;
+    for(int mask = 0;mask<(1<<20);mask++) {
+        vector<int> has_bit;
+        for(int i = 0;i<20;i++) if(mask&(1<<i)) has_bit.push_back(i);
+        for(int i = 0;i<20;i++){
+            if(mask&(1<<i)) continue;
+            long long sum = 0;
+            for(int j = 0;j<has_bit.size();j++) sum+=value[i][has_bit[j]];
+            int nmask = mask|(1<<i);
+            ans[nmask] = min(ans[nmask], ans[mask]+sum);
+        }
+    }
+    printf("%lld\n", ans[(1<<20)-1]);
+    return 0;
 }
+
+
