72f6bfb9e85aed9aad4ec8420134794c84a68607
xuzinan <xuzinan@bytedance.com>
2019-10-06 13:39:05 +0800

    del 226B.cpp nouse
    Change-Id: Id83068ea4c2817cc78f89355290dca591c1d3cfe

--- 226/B.cpp
+++ 226/B.cpp
@@ -4,9 +4,7 @@ const int maxm = 1e5+10;
 int n, q;
 long long a[maxm];
 void input() {
-	//scanf("%d",&n);
 	cin>>n;
-	//for(int i = 0;i<n;i++) scanf("%lld", a+i);
 	for(int i = 0;i<n;i++) cin>>a[i];
 	sort(a,a+n);
 	reverse(a,a+n);
@@ -17,7 +15,6 @@ long long get_ans(int len) {
 	long long rlen = len, cnt = 1;
 	for(int i = 1;i<n;) {
 		int r = min(n-1ll,i+rlen-1);
-		//cout<<i<<' '<<r<<' '<<cnt<<endl;
 		ans += (a[r] - a[i-1])*cnt;
 		cnt++;
 		i = r+1;
