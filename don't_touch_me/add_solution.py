22c6813dc683c3f7922ae65d10f5f88ac5362e8f
xuzinan <xuzinan@bytedance.com>
2019-10-12 14:24:37 +0800

    add solution_list
    Change-Id: I25d643bb62494c19c97fb7e5290140a39c5c2c37

--- don't_touch_me/add_solution.py
+++ don't_touch_me/add_solution.py
@@ -1,13 +1,33 @@
 import sys
-if __name__ == "__main__":
-	if len(sys.argv) != 3:
-		print("please give me problem ID")
-		exit(0)
-	contest_id = str(sys.argv[1])
-	problem_id = str(sys.argv[2])
+import os
+def transfer(problem) :
+	for ind in range(len(problem)):
+		if problem[ind] not in '0123456789':
+			return problem[:ind], problem[ind:]
+	exit(0)
+
+def add(contest_id, problem_id):
 	with open('../README.md', 'a+') as f:
 		contest_name = '[' + contest_id + problem_id + ']'
 		contest_link = '(https://codeforces.com/problemset/problem/' + contest_id + '/' + problem_id + ')'
 		github_name = '[solution]'
 		github_link = '(https://github.com/zuhiul/codeforce-solution/blob/master/' + contest_id + '/' + problem_id + '.cpp)'
 		f.write(contest_name + contest_link + ' : ' + github_name + github_link + '\n\n')
+
+if __name__ == '__main__':
+	if len(sys.argv) != 2:
+		print("please give me the path")
+		exit(0)
+	filePath = str(sys.argv[1])
+	problem_list = []
+	for i,j,k in os.walk(filePath):
+		if 'cpp' not in ''.join(k):
+			continue
+		last_ind = i.rfind('/')
+		for name in k:
+			ind = name.rfind('.')
+			problem_list.append(i[last_ind+1:] + name[:ind])
+	problem_list.sort()
+	for problem in problem_list:
+		contest_id, problem_id = transfer(problem)
+		add(contest_id, problem_id)
